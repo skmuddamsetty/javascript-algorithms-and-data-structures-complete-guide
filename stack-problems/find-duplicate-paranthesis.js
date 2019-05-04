794e6416ee3d579aeb957afdba56cf8e5403cf7e
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-05-04 08:00:52 -0400

    dup paranthesis

--- stack-problems/find-duplicate-paranthesis.js
+++ stack-problems/find-duplicate-paranthesis.js
@@ -55,5 +55,34 @@
     return false;
   }
 
+  /**
+   * Method 2 to find dup paranthesis
+   * @param {string} str input str
+   * @returns {boolean} true if dup paranthesis present, else false
+   */
+  function findDupParanthesis(str) {
+    let stack = [];
+    for (let char of str) {
+      let elemCount = 0;
+      if (char !== ')') {
+        stack.push(char);
+      } else {
+        if (stack.length) {
+          while (stack.length && stack[stack.length - 1] !== '(') {
+            elemCount++;
+            stack.pop();
+          }
+          if (elemCount < 1) {
+            return true;
+          }
+          stack.pop(); // this pop is to remove the matching opening brace for the current processing closing brace
+        }
+      }
+    }
+    return false;
+  }
+
+  findDupParanthesis('((a+(b))+(c+d))');
+
   findDuplicateParanthesis('(a)');
 }
