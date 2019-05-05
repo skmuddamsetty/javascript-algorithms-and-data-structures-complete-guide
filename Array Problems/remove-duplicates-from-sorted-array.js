db12a30aa33443b40e2f76ed24dfbc08c51563d3
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-05-05 08:28:46 -0400

    remove duplcates

--- Array Problems/remove-duplicates-from-sorted-array.js
+++ Array Problems/remove-duplicates-from-sorted-array.js
@@ -25,5 +25,56 @@
     return distinctArr;
   }
 
+  /**
+   * Removes the duplicates in the array without using extra space
+   * @param {Array} arr input array
+   * @returns {Array}
+   * Time Complexity: O(n)
+   * Space Complexity: O(1)
+   */
+  function removeDuplicatesFromSortedArrayWithoutExtraSpace(arr = []) {
+    let j = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] !== arr[i + 1]) {
+        j = j + 1;
+        arr[j] = arr[i + 1];
+      }
+    }
+    j = j + 1;
+    while (j < arr.length) {
+      arr[j] = null;
+      j++;
+    }
+    return arr;
+  }
+
+  removeDuplicatesFromSortedArrayWithoutExtraSpace([
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    2,
+    2,
+    2,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    4,
+    4,
+    4,
+    4,
+    4,
+    4,
+    4,
+    4,
+    4
+  ]);
+
   removeDuplicatesFromSortedArray([1, 2, 2, 3, 3, 3]);
 }
